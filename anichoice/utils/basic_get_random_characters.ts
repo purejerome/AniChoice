8ab53696f92523554c218644e8bdc77d29cc4314
Jerome <donfacjc@dukes.jmu.edu>
2025-08-15 15:49:49 -0400

    made the base_get_random_chars into a function that can time out after a while, which is what i should have done from the start lol

--- anichoice/utils/basic_get_random_characters.ts
+++ anichoice/utils/basic_get_random_characters.ts
@@ -8,32 +8,33 @@ const randomClient = new RandomClient({
     }
 });
 
-export default async function base_get_random_characters() {
-    const characters_array: SimpleCharacter[] = [];
-    let slots_filled = 0;
-    while (slots_filled < 20) {
-        const batchSize = 5;
-        const results = await Promise.all(
-            Array.from({ length: batchSize }, () => randomClient.getRandomCharacters())
-        );
-        console.log("results", results);
-        for(const character of results) {
-            const new_character = {
-                id: character.data.mal_id,
-                name: character.data.name,
-                image: character.data.images.jpg.image_url,
-                name_kanji: character.data.name_kanji
-            };
-            if (!characters_array.find(c => c.id === new_character.id) 
-                && !new_character.image.includes("apple-touch-icon")) {
-                characters_array.push(new_character);
-                slots_filled++;
-                if (characters_array.length >= 20) break;
+export default function base_get_random_characters() {
+    return new Promise<SimpleCharacter[]>(async (resolve, reject) => {
+        const characters_array: SimpleCharacter[] = [];
+        let slots_filled = 0;
+        const timer = setTimeout(() => reject(new Error("Took too long to find characters")), 40000);
+        while (slots_filled < 20) {
+            const batchSize = 5;
+            const results = await Promise.all(
+                Array.from({ length: batchSize }, () => randomClient.getRandomCharacters())
+            );
+            console.log("results", results);
+            for(const character of results) {
+                const new_character = {
+                    id: character.data.mal_id,
+                    name: character.data.name,
+                    image: character.data.images.jpg.image_url,
+                    name_kanji: character.data.name_kanji
+                };
+                if (!characters_array.find(c => c.id === new_character.id) 
+                    && !new_character.image.includes("apple-touch-icon")) {
+                    characters_array.push(new_character);
+                    slots_filled++;
+                    if (characters_array.length >= 20) break;
+                }
             }
         }
-        console.log(slots_filled)
-        console.log(characters_array)
-    }
-    console.log("Final array:", characters_array.map(c => c.name));
-    return characters_array;
+        clearTimeout(timer);
+        resolve(characters_array);
+    });
 }
